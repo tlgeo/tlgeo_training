a706cd7e0fcd217820f3952f6c9498f40b1fe40d
luantm <ngoalongtb001@gmail.com>
2021-12-10 19:13:44 +0900

    add training 4

---
+++ algorithm/oj.vnoi.info/liq.cpp
@@ -0,0 +1,8 @@
+#include "iostream"
+
+int main() {
+
+    std::cout << "Xin chao cac ban\n";
+
+    return 0;
+}
---
+++ algorithm/oj.vnoi.info/post.cpp
@@ -0,0 +1,15 @@
+// A cộng B
+// input: Gồm  dòng chứa  số nguyên  A và B , cách bởi  dấu cách.
+// output: Ghi ra tổng A + B.
+
+#include "iostream"
+
+int main() {
+    int a, b;
+    // Minh se viet theo cu phap cua C (khong phai C++);
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    printf("%d", a + b);
+    return 0;
+}
